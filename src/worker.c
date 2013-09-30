bcd84980d70c84b2ec68fd8884a9564e2fda82b3
Paul Colomiets <paul@colomiets.name>
2013-09-30 19:48:56 +0300

    Implement most of the worker code (untested though)

--- src/worker.c
+++ src/worker.c
@@ -25,8 +25,15 @@
 
 #include "utils/thread.h"
 #include "utils/err.h"
+#include "utils/random.h"
 #include "state.h"
 
+/*  Amount of ms to wait if nn_send failed or reply is invalid  */
+#define NC_ERROR_RETRY_TIME 100
+/*  Time for reply. 1 sec should be enough nowadays  */
+#define NC_REQUEST_WAIT_TIME 1000
+/*  Check for NS record updates every 5 minutes  */
+#define NC_REQUEST_AGAIN_TIME (5*60*1000)
 
 enum {
     NC_POLL_CMD_RCVFD,
@@ -35,14 +42,56 @@ enum {
     NC_POLL_NUM
 };
 
+enum {
+    NC_STATE_STARTING,
+    NC_STATE_REQUEST_SENT,
+    NC_STATE_SLEEPING,
+};
+
+
+struct nc_topic {
+
+    struct nc_subscr_list {
+        struct nc_subscription *head;
+        struct nc_subscription **tail;
+    } subscr_list;
+
+    int topic_len;
+    char topic[];
+};
+
+
+struct nc_subscription {
+    struct nn_socket_list {
+        struct nc_subscription **prev;
+        struct nc_subscription *next;
+    } socket_list;
+    struct nn_topic_list {
+        struct nc_subscription **prev;
+        struct nc_subscription *next;
+    } topic_list;
+
+    struct nc_topic *topic;
+    struct nc_socket *socket;
+};
+
+
 struct nc_socket {
     int socket_no;
+    int state;
+    uint32_t request_id;
     uint64_t sent_time;
     uint64_t retry_time;
 
     struct nc_socket **prev;
     struct nc_socket *next;
 
+    struct nc_subscr_list {
+        struct nc_subscription *head;
+        struct nc_subscription **tail;
+    } subscr_list;
+
+    /*  A buffer for name request  */
     int request_len;
     char request[];
 };
@@ -55,14 +104,19 @@ struct nc_worker {
     int updates_socket;
     int updates_rcvfd;
     int running;
+    uint32_t next_request_id;
 
     /*  TODO(tailhook) add ifdefs for Windows */
     struct pollfd fds[NC_POLL_NUM];
 
-    struct nn_socket_list {
+    struct nc_socket_list {
         struct nc_socket *head;
         struct nc_socket **tail;
     } socket_list;
+    struct nc_topic_list {
+        struct nc_topic *head;
+        struct nc_topic **tail;
+    } topic_list;
 };
 
 static struct nc_worker worker_struct;
@@ -176,11 +230,12 @@ static void nc_process_configure (struct nc_worker *self,
     int constval;
     int constlen;
     char *nnconst;
+    char *target;
 
     sock = nc_list_find (self, cmd->socket)
 
     if (sock) {
-
+        sock->state = NC_STATE_STARTING;  /*  Will resend request ASAP  */
     } else {
         optlen = sizeof(proto);
 
@@ -196,18 +251,30 @@ static void nc_process_configure (struct nc_worker *self,
         }
 
         constlen = strlen(nconst);
-        reqlen = urllen = strlen(cmd->url);
+
+        urllen = strlen(cmd->url);
+        reqlen = 4;  /*  Request id  */
+        reqlen += strlen("RESOLVE ");
+        reqlen += urllen;
         reqlen += 1;  /*  A space  */
         reqlen += constlen;
 
-
         sock = nn_alloc (sizeof (struct nc_socket) + reqlen, "socket_str");
+        sock->state = NC_STATE_STARTING;
         sock->socket_no = cmd->socket;
-        /*  The request looks like "URL NN_SOCK_TYPE" */
-        memcpy (sock->request, urllen);
-        sock->request [urllen] = ' ';
-        memcpy (sock->request [urllen + 1], nnconst, constlen);
+        /*  The request looks like:
+            REQUEST_ID(4 bytes) + "RESOLVE " + URL + " " + NN_SOCK_TYPE
+            */
+        target = sock->request + 4;  /*  Requestid is filled later  */
+        memcpy (target, "RESOLVE ", strlen("RESOLVE "));
+        target += strlen("RESOLVE ");
+        memcpy (target, cmd->url, urllen);
+        target += urllen;
+        *target++ = ' ';
+        memcpy (target, nnconst, constlen);
         sock->request_len = reqlen;
+        sock->subscr_list.head = NULL;
+        sock->subscr_list.tail = &sock->subscr_list.head;
 
         nc_list_add (self, sock);
     }
@@ -223,11 +290,12 @@ static void nc_process_close (struct nc_worker *self,
         /*  Allow not configured sockets to close this is to avoid too much
             checking for which sockets are configured by nanoconfig and which
             aren't by user  */
-        nn_close(cmd->socket);
+        nn_close (cmd->socket);
         return;
     }
 
     nc_list_rm (self, sock);
+    nn_close (cmd->socket);
 }
 
 
@@ -268,6 +336,129 @@ static void nc_process_commands (struct nc_worker *self)
     }
 }
 
+static void nc_parse_and_apply (struct nc_worker *worker,
+    struct nc_socket *self, char *buf, int buflen) {
+    assert (0);
+}
+
+static void nc_process_responses (struct nc_worker *self) {
+    char *buf;
+    int buflen;
+    int rc
+    struct nn_socket *item;
+
+    while(1) {
+        buflen = nn_recv (self->request_socket, &buf, NN_MSG, NN_DONTWAIT);
+
+        if (buflen < 0) {
+            errno_assert (errno == EAGAIN);
+            return;
+        }
+
+        if (buflen < 5)
+            continue;  /*  Non-valid reply  */
+        request_id = *(uint32_t *)buf;
+        if (!(request_id & 0x80000000u))
+            continue;  /*  We're not endpoint for this message  */
+        request_id &= ~0x80000000u;
+        for (item = self->socket_list.head; item; item = item->next) {
+            if (item->request_id == request_id) {
+                if (item->state != NC_STATE_REQUEST_SENT)
+                    break;  /*  Already has a reply or request reset  */
+                item->state = NC_STATE_SLEEPING;
+                if (nc_parse_and_apply (self, item, buf, buflen)) {
+                    item->retry_time = nc_clock_now (worker->clock) +
+                        NC_REQUEST_AGAIN_TIME;
+                } else {
+                    item->retry_time = nc_clock_now (worker->clock) +
+                        NC_ERROR_RETRY_TIME;
+                }
+                break;
+            }
+        }
+    }
+}
+
+static void nc_process_updates (struct nc_worker *self) {
+    char *buf;
+    int buflen;
+    int rc;
+    struct nn_socket *item;
+    struct nn_subscription *sub;
+    struct nn_topic *topic;
+
+    while(1) {
+        buflen = nn_recv (self->updates_socket, &buf, NN_MSG, NN_DONTWAIT);
+
+        if (buflen < 0) {
+            if (errno == EAGAIN)
+                return;
+            if (errno == EINTR || errno == ETERM)
+                /*  Exit immediately if we shutting down,
+                    but return shortly if not  */
+                return;
+            errno_assert (0);
+        }
+
+        for (topic = self->topic_list.head; topic; topic = topic->next) {
+            if (buflen < topic->topic_len)
+                continue;
+            if (!memcmp (topic->topic, buf, topic->topic_len)) {
+                for (sub = topic->subscr_list.head; sub; sub = item->next) {
+                    sub->socket->state = NC_STATE_STARTING;
+                }
+            }
+        }
+    }
+}
+
+static uint64_t nc_process_timers (struct nn_worker *self) {
+    int rc;
+    uint64_t now;
+    uint64_t next_timeout;
+
+    next_timeout = now + 60000;
+    now = nn_clock_now (self);
+
+    for (item = self->socket_list.head; item; item = item->next) {
+        if (item->state != NC_STATE_STARTING && item->retry_time < now) {
+            item->state = NC_STATE_STARTING;
+        } else {
+            next_timeout = min (next_timeout, item->retry_time);
+        }
+        if (item->state == NC_STATE_STARTING) {
+            item->request_id = self->next_request_id;
+            self->next_request_id += 1;
+            if (self->next_request_id == 0x80000000u) {
+                self->next_request_id = 0;
+            *(uint32_t *)item->request =
+                htonl(self->next_request_id) | 0x80000000u;
+            rc = nn_send (self->request_socket,
+                item->request, item->request_len,
+                NN_DONTWAIT);
+            if (rc < 0) {
+                if (errno == EINTR || errno == ETERM)
+                    /*  Exit immediately if we shutting down,
+                        but return shortly if not  */
+                    return 0;
+                if (errno == EAGAIN) {
+                    item->retry_time = now + NC_ERROR_RETRY_TIME;
+                    item->state = NC_STATE_SLEEPING;
+                    next_timeout = min (next_timeout, item->retry_time);
+                } else {
+                    errno_assert (0);
+                }
+            }
+            item->state = NC_REQUEST_SENT;
+            item->retry_type = NC_REQUEST_WAIT_TIME
+        }
+    }
+    now = nn_clock_now (self);
+    if (next_timeout > now)
+        return now - next_timeout;
+    return 0;
+}
+
 static void nc_worker_loop (void *data)
 {
     struct nc_worker *self;
@@ -282,7 +473,7 @@ static void nc_worker_loop (void *data)
 
     while (1) {
 
-        timeo = nc_process_timeouts (self);
+        timeo = nc_process_timers (self);
 
         if (nc_poll_exec (self, timeo) > 0) {
 
@@ -314,13 +505,17 @@ static void nc_worker_loop (void *data)
 
 void nc_worker_start(struct nc_state *state) {
     struct nc_worker *self;
+    nn_random_seed();
 
     self = &worker_struct;
     self->request_socket = state->request_socket;
     self->update_socket = state->update_socket;
     self->socket_list.head = NULL;
     self->socket_list.tail = &self->socketlist.head;
+    self->topic_list.head = NULL;
+    self->topic_list.tail = &self->socketlist.head;
     self->running = 1;
+    nn_random_generate (&self->next_request_id, sizeof(self->next_request_id));
 
     nn_thread_init (&state->worker, nc_worker_loop, self);
 
